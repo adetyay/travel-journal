ad3113c1500f6efd9dc2fe4a9e56bb7c2e2851a9
aditya <129599872+adityax4@users.noreply.github.com>
2024-07-03 14:34:45 +0530

    fix styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="navbar rounded-t-xl">
       <img src="world-icons/icons8-world-100.png" />
-      <h1 className="text-white text-lg font-semibold">my travel journal.</h1>
+      <h1 className="text-white text-lg font-bold">my travel journal.</h1>
     </nav>
   );
 }
