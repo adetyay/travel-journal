f0985eabde98753a442d7a11544ae2d653e76222
aditya <129599872+adityax4@users.noreply.github.com>
2024-07-03 14:04:35 +0530

    v1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="app rounded-xl shadow-xl">
+    <div className="app rounded-xl shadow-2xl">
       <Navbar />
       <Hero />
     </div>
